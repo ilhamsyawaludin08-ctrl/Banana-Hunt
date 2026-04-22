5f25336fda97c1096c28bd8c39fb22f0024751be
ilhamsyawaludin08-ctrl <ilhamsyawaludin08@gmail.com>
2026-04-22 19:00:48 +0700

    mentriger tombol play ke scene gameplay

--- Assets/Banana Hunt/scripts/main/MainMenu/Buttmanager.cs
+++ Assets/Banana Hunt/scripts/main/MainMenu/Buttmanager.cs
@@ -6,19 +6,22 @@ public class Buttmanager : MonoBehaviour
     public void BukaMainmenu()
     {
         SceneManager.LoadScene("Mainmenu");
-
     }
 
     public void Bukasettings()
     {
         SceneManager.LoadScene("settings");
-
     }
 
     public void GoToExit()
     {
-        Debug.Log ("BERHASIL KELUAR APLIKASI");
-        Application.Quit ();
+        Debug.Log("BERHASIL KELUAR APLIKASI");
+        Application.Quit();
     }
 
+    // 🔥 TAMBAHAN INI DOANG
+    public void BukaGameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
 }
