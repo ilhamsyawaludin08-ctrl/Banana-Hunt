b2259d2be298715550d0e36af8a4800875e297df
mikel32165361 <mukhalazi@gmail.com>
2026-05-19 23:58:52 +0700

    nambah button exit

--- Assets/Banana Hunt/scripts/main/MainMenu/Buttmanager.cs
+++ Assets/Banana Hunt/scripts/main/MainMenu/Buttmanager.cs
@@ -3,25 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class Buttmanager : MonoBehaviour
 {
+    // Buka Main Menu
     public void BukaMainmenu()
     {
         SceneManager.LoadScene("Mainmenu");
     }
 
+    // Buka Settings
     public void Bukasettings()
     {
         SceneManager.LoadScene("settings");
     }
 
-    public void GoToExit()
-    {
-        Debug.Log("BERHASIL KELUAR APLIKASI");
-        Application.Quit();
-    }
-
-    // 🔥 TAMBAHAN INI DOANG
+    // Buka Gameplay
     public void BukaGameplay()
     {
         SceneManager.LoadScene("Gameplay");
+    }
+
+    // Exit Game
+    public void GoToExit()
+    {
+        Debug.Log("BERHASIL KELUAR APLIKASI");
+
+        // Menutup game
+        Application.Quit();
+
+        // Khusus untuk testing di Unity Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
